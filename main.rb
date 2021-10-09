808e1669bdcdebfe731f44a9045e80bbe5e28f68
masakiq <>
2021-10-09 16:10:13 +0900

    Validate App proxy requests

--- main.rb
+++ main.rb
@@ -4,7 +4,15 @@ require 'openssl'
 require 'sinatra'
 require 'httparty'
 require 'jwt'
+require 'rack/utils'
 require 'pry'
+
+class InvalidProxyRequest < StandardError; end
+
+not_found do
+  response.headers['Content-Type'] = 'application/liquid'
+  'not found'
+end
 
 get '/' do
   puts request.body.rewind
@@ -24,7 +32,7 @@ get '/' do
 end
 
 get '/callback' do
-  # 検証処理はスキップ
+  # skip verification
   puts request.body.rewind
   code = params['code']
   shop = params['shop']
@@ -44,7 +52,12 @@ get '/callback' do
 end
 
 get '/proxy/' do
-  puts request.query_string
+  begin
+    verify_proxy_signature(request.query_string)
+  rescue InvalidProxyRequest
+    response.headers['Content-Type'] = 'application/liquid'
+    return 'Invalid Proxy Request'
+  end
 
   puts '***** REQUEST PARAMS *****'
   params.each do |key, val|
@@ -58,7 +71,13 @@ get '/proxy/' do
 end
 
 get '/proxy/customer_token' do
-  # 検証処理はスキップ
+  begin
+    verify_proxy_signature(request.query_string)
+  rescue InvalidProxyRequest
+    response.headers['Content-Type'] = 'application/liquid'
+    return 'Invalid Proxy Request'
+  end
+
   jwt = encode_jwt(params['customer_id'], params['shop'])
 
   response.headers['Content-Type'] = 'application/liquid'
@@ -73,7 +92,13 @@ get '/proxy/customer_token' do
 end
 
 get '/proxy/customer_info' do
-  # 検証処理はスキップ
+  begin
+    verify_proxy_signature(request.query_string)
+  rescue InvalidProxyRequest
+    response.headers['Content-Type'] = 'application/liquid'
+    return 'Invalid Proxy Request'
+  end
+
   response.headers['Content-Type'] = 'application/json'
   begin
     customer_token = decode_jwt(params['customer_token'])
@@ -91,6 +116,13 @@ get '/proxy/customer_info' do
 end
 
 get '/proxy/account' do
+  begin
+    verify_proxy_signature(request.query_string)
+  rescue InvalidProxyRequest
+    response.headers['Content-Type'] = 'application/liquid'
+    return 'Invalid Proxy Request'
+  end
+
   response.headers['Content-Type'] = 'application/liquid'
   <<~HTML
     <body>
@@ -177,6 +209,20 @@ get '/proxy/account' do
       document.getElementById('clearCustomerInfo').addEventListener('click', clearCustomerInfo);
     </script>
   HTML
+end
+
+def verify_proxy_signature(query_string)
+  puts query_string
+
+  query_hash = Rack::Utils.parse_query(query_string)
+  signature = query_hash.delete('signature')
+  sorted_params = query_hash.collect { |k, v| "#{k}=#{Array(v).join(',')}" }.sort.join
+  calculated_signature = OpenSSL::HMAC.hexdigest(
+    OpenSSL::Digest.new('sha256'),
+    ENV['SHOPIFY_APP_API_SECRET_KEY'],
+    sorted_params
+  )
+  raise InvalidProxyRequest unless signature == calculated_signature
 end
 
 def encode_jwt(customer_id, shop)
